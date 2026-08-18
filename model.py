682b777d14f2dea7e7cc1928867846abef0dbcb9
Erik Wagner <erikwagner3@gmail.com>
2026-08-17 21:52:20 -0700

    Optimize prediction model for web deployment

--- model.py
+++ model.py
@@ -202,7 +202,12 @@ def model_train(features, target, combined_data):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     #training the model
-    model = RandomForestRegressor()
+    model = RandomForestRegressor(
+    n_estimators=25,
+    max_depth=12,
+    random_state=42,
+    n_jobs=1
+)
     model.fit(X_train, y_train)
     
     #model score is essentially r^2
